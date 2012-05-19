fa4e12f8870ca8583b48c3e3f6d34147d380dda4
Double0negative <soccerkid90@earthlink.net>
2012-05-19 03:58:21 -0400

    B 0.3.3

--- src/com/skitscape/survivalgames/LobbyManager.java
+++ src/com/skitscape/survivalgames/LobbyManager.java
@@ -335,7 +335,7 @@ public class LobbyManager  implements Listener{
                 }
                 for(Player p:inactive){
                     if(signno<signs[b].length){
-                        signs[b][signno].setLine(line, (SurvivalGames.auth.contains(p.getName())?ChatColor.DARK_BLUE:ChatColor.BLACK) +((p.getName().equalsIgnoreCase("Double0negative"))?"Double0":p.getName()));
+                        signs[b][signno].setLine(line, (SurvivalGames.auth.contains(p.getName())?ChatColor.DARK_RED:ChatColor.GRAY) +((p.getName().equalsIgnoreCase("Double0negative"))?"Double0":p.getName()));
                         signs[b][signno].update();
                         line++;
                         if(line == 4){
